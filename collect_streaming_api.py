2990533cd61b2fabf88a812a27edca453ddae146
Antonio Carlos <baptistaacbc@hotmail.com>
2018-05-06 12:41:21 -0300

    Adicionado Search + Streaming API. Agora é possível resgatar os ultimos tweets relacionados a eleição e também verificar uma pesquisa por trend topics do mesmo tema

---
+++ collect_streaming_api.py
@@ -0,0 +1,31 @@
+#Streaming APIs give access to (usually a sample of) all tweets as they published on Twitter.
+#The Streaming API only sends out real-time tweets
+
+# Import JSON to deal with twitter wrapper output
+import json
+import tokens as tokens
+
+# Importing twitter wrapper library
+from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+
+oauth = OAuth(tokens.ACCESS_TOKEN, tokens.ACCESS_SECRET, tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET)
+
+# Initiate the connection to Twitter Streaming API
+twitter_stream = TwitterStream(auth=oauth)
+
+# Get a sample of the public data following through Twitter
+#iterator = twitter_stream.statuses.sample()
+#iterator = twitter_stream.statuses.filter(track="Google", language="en")
+iterator = twitter_stream.statuses.filter(track='eleição', language="pt")
+
+tweet_count = 10
+for tweet in iterator:
+    tweet_count -= 1
+    # if 'delete' in tweet:
+    #     continue
+        #tweet_count +=1
+    #print(json.dumps(tweet, indent=4))
+    print(json.dumps(tweet))
+
+    if tweet_count <= 0:
+        break
