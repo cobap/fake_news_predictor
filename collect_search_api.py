ee52affee56aced536aaca8ea3f575d5c3ef15f2
Antonio Carlos <baptistaacbc@hotmail.com>
2018-05-20 22:12:47 -0300

    Adicionado compatibilidade com PEP8

--- collect_search_api.py
+++ collect_search_api.py
@@ -1,14 +1,19 @@
-#Streaming APIs give access to (usually a sample of) all tweets as they published on Twitter.
-#The Streaming API only sends out real-time tweets
+"""
+Streaming APIs give access to (usually a sample of) all tweets as
+they published on Twitter.
+"""
+# The Streaming API only sends out real-time tweets
 
 # Import JSON to deal with twitter wrapper output
-import json
+# import json
 import tokens as tokens
 
 # Importing twitter wrapper library
-from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+# from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+from twitter import Twitter, OAuth
 
-oauth = OAuth(tokens.ACCESS_TOKEN, tokens.ACCESS_SECRET, tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET)
+oauth = OAuth(tokens.ACCESS_TOKEN, tokens.ACCESS_SECRET, tokens.CONSUMER_KEY,
+              tokens.CONSUMER_SECRET)
 
 # Initiate the connection to Twitter REST API
 twitter = Twitter(auth=oauth)
@@ -18,13 +23,13 @@ twitter = Twitter(auth=oauth)
 
 # Get all the locations where Twitter provides trends service
 # Brazil 23424768
-#world_trends = twitter.trends.available(_woeid=23424768)
+# world_trends = twitter.trends.available(_woeid=23424768)
 # Brasilia 455819
-#world_trends = twitter.trends.available(_woeid=455819)
+# world_trends = twitter.trends.available(_woeid=455819)
 # Sao Paulo 455827
-#world_trends = twitter.trends.available(_woeid=455827)
+# world_trends = twitter.trends.available(_woeid=455827)
 
-sfo_trends = twitter.trends.place(_id = 23424768)
+sfo_trends = twitter.trends.place(_id=23424768)
 
 # print(json.dumps(sfo_trends, indent=4))
-#print(json.dumps(twitter.application.rate_limit_status(), indent=4))
+# print(json.dumps(twitter.application.rate_limit_status(), indent=4))
--- collect_streaming_api.py
+++ collect_streaming_api.py
@@ -1,21 +1,26 @@
-#Streaming APIs give access to (usually a sample of) all tweets as they published on Twitter.
-#The Streaming API only sends out real-time tweets
+"""
+Streaming APIs give access to (usually a sample of) all tweets as
+they published on Twitter.
+"""
+# The Streaming API only sends out real-time tweets
 
 # Import JSON to deal with twitter wrapper output
 import json
 import tokens as tokens
 
 # Importing twitter wrapper library
-from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+# from twitter import Twitter, OAuth, TwitterHTTPError, TwitterStream
+from twitter import OAuth, TwitterStream
 
-oauth = OAuth(tokens.ACCESS_TOKEN, tokens.ACCESS_SECRET, tokens.CONSUMER_KEY, tokens.CONSUMER_SECRET)
+oauth = OAuth(tokens.ACCESS_TOKEN, tokens.ACCESS_SECRET, tokens.CONSUMER_KEY,
+              tokens.CONSUMER_SECRET)
 
 # Initiate the connection to Twitter Streaming API
 twitter_stream = TwitterStream(auth=oauth)
 
 # Get a sample of the public data following through Twitter
-#iterator = twitter_stream.statuses.sample()
-#iterator = twitter_stream.statuses.filter(track="Google", language="en")
+# Iterator = twitter_stream.statuses.sample()
+# Iterator = twitter_stream.statuses.filter(track="Google", language="en")
 iterator = twitter_stream.statuses.filter(track='eleição', language="pt")
 
 tweet_count = 10
@@ -23,8 +28,8 @@ for tweet in iterator:
     tweet_count -= 1
     # if 'delete' in tweet:
     #     continue
-        #tweet_count +=1
-    #print(json.dumps(tweet, indent=4))
+    # tweet_count +=1
+    # print(json.dumps(tweet, indent=4))
     print(json.dumps(tweet))
 
     if tweet_count <= 0:
